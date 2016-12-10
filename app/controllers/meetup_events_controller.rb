312d16113c896a0299beb345c65da37eb7041ef7
Andrei Aberto <luckysevenbear@gmail.com>
2016-12-10 04:14:07 +0100

    added search feature

--- app/controllers/meetup_events_controller.rb
+++ app/controllers/meetup_events_controller.rb
@@ -6,22 +6,37 @@ class MeetupEventsController < ApplicationController
   end
 
   def list
+    zipcode = params[:filter_options][:zipcode]
+    radius = params[:filter_options][:radius]
+
     meetup_api = MeetupApi.new
-    params = { category: '9',
-               format: 'json',
-               page: '50'}
+    params = {
+        category: '9',
+        format: 'json',
+        page: '30',
+        radius: zipcode,
+        zip: radius
+    }
     fitness_events = meetup_api.open_events(params)
     @fitness_events = fitness_events['results']
 
-    params = { category: '23',
-               format: 'json',
-               page: '50'}
+    params = {
+        category: '23',
+        format: 'json',
+        page: '30',
+        radius: zipcode,
+        zip: radius
+    }
     outdoor_events = meetup_api.open_events(params)
     @outdoor_events = outdoor_events['results']
 
-    params = { category: '32',
-               format: 'json',
-               page: '50'}
+    params = {
+        category: '32',
+        format: 'json',
+        page: '30',
+        radius: zipcode,
+        zip: radius
+    }
     sports_events = meetup_api.open_events(params)
     @sports_events = sports_events['results']
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "meetup_events#index"
   get "meetup_events/list"
+  post "meetup_events/list", :as => :meetup_events_search
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
