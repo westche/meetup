abae2af392d6c10dae0ace7b4a301a7bc32cdd90
Andrei Aberto <luckysevenbear@gmail.com>
2016-12-12 11:07:07 +0100

    moved events list on main page

--- app/controllers/meetup_events_controller.rb
+++ app/controllers/meetup_events_controller.rb
@@ -1,8 +1,45 @@
 class MeetupEventsController < ApplicationController
   def index
+    # meetup_api = MeetupApi.new
+    # result = meetup_api.categories({})
+    # @categories = result['results']
+    zipcode = 90001
+    radius = 50
+    if params[:filter_options].present?
+      zipcode = params[:filter_options][:zipcode]
+      radius = params[:filter_options][:radius]
+    end
+
     meetup_api = MeetupApi.new
-    result = meetup_api.categories({})
-    @categories = result['results']
+    params = {
+        category: '9',
+        format: 'json',
+        page: '10',
+        radius: radius,
+        zip: zipcode
+    }
+    fitness_events = meetup_api.open_events(params)
+    @fitness_events = fitness_events['results']
+
+    params = {
+        category: '23',
+        format: 'json',
+        page: '10',
+        radius: radius,
+        zip: zipcode
+    }
+    outdoor_events = meetup_api.open_events(params)
+    @outdoor_events = outdoor_events['results']
+
+    params = {
+        category: '32',
+        format: 'json',
+        page: '10',
+        radius: radius,
+        zip: zipcode
+    }
+    sports_events = meetup_api.open_events(params)
+    @sports_events = sports_events['results']
   end
 
   def list
@@ -43,5 +80,13 @@ class MeetupEventsController < ApplicationController
     }
     sports_events = meetup_api.open_events(params)
     @sports_events = sports_events['results']
+  end
+
+  def join
+    params = {
+        category: '32',
+        format: 'json',
+        page: '30'
+    }
   end
 end
